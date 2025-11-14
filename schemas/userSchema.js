43798d5b9840a222a60ec7eebd7d6b3817acda35
25LP8016 25LP8016 <25LP8016@G305-CC0243.local>
2025-11-14 12:33:22 +0800

    Update

---
+++ schemas/userSchema.js
@@ -0,0 +1,17 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: [true, "First name is required"],
+    minLength: [2, "First name length must be at least 2 characters"],
+    maxLength: [50, "First name length must be 6 to 50 characters"],
+  },
+  email: {
+    type: String,
+    required: [true],
+  },
+});
+
+const UserModel = model("User", UserSchema);
+module.exports = UserModel;
